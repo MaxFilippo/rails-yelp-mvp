e03afb80eaecd08d9cd82e46c39cd5645d59efc9
Maximilian Eich <maximilian.f.d.eich@gmail.com>
2022-08-17 16:15:58 +0200

    keine Ahnung was der hier gemacht hat

---
+++ db/migrate/20220817110511_create_restaurants.rb
@@ -0,0 +1,13 @@
+class CreateRestaurants < ActiveRecord::Migration[7.0]
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.string :address
+      t.string :category
+      t.string :phone_number
+      t.integer :review
+
+      t.timestamps
+    end
+  end
+end
